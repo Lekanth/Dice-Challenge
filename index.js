3307041f711d1611b62524150674882a3458c9ee
lekanth hy <119438917+Lekanth@users.noreply.github.com>
2024-09-02 01:03:58 +0530

    index.js

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var random=Math.floor(Math.random() * 6) + 1;
 var randp="dice"+ random +".png";
-var randimg="images/"+randp;
 var img11=document.querySelectorAll("img")[0];
-img11.setAttribute("src",randimg);
+img11.setAttribute("src",randp);
 var random1=Math.floor(Math.random() * 6) + 1;
 var randp="dice"+ random1 +".png";
-var randimg1="images/"+randp;
 var img22=document.querySelectorAll("img")[1];
-img22.setAttribute("src",randimg1);
+img22.setAttribute("src",randp);
 
 if(random>random1)
 {
